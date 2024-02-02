d014f598d57ac5839b2f89ff006ef85d1b98c913
Hugo VO Moura <zoeiratheforever@gmail.com>
2024-02-02 15:25:05 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/hugovitoroliveira/project/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.hugovitoroliveira.project.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hugovitoroliveira.project.entities.Order;
+import com.hugovitoroliveira.project.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	public Order findById(Long Id) {
+		Optional<Order> obj = repository.findById(Id);
+		return obj.get();
+	}
+}

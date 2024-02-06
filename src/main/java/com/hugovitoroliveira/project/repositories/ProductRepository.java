bd65a5713e39fdfd21d860ee90229a5371af23b4
Hugo VO Moura <zoeiratheforever@gmail.com>
2024-02-06 14:47:57 -0300

    Product Entity

---
+++ src/main/java/com/hugovitoroliveira/project/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.hugovitoroliveira.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hugovitoroliveira.project.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
---
+++ src/main/java/com/hugovitoroliveira/project/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.hugovitoroliveira.project.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hugovitoroliveira.project.entities.Product;
+import com.hugovitoroliveira.project.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	public Product findById(Long Id) {
+		Optional<Product> obj = repository.findById(Id);
+		return obj.get();
+	}
+}
